74efb6f2f2337a6997159b31d7d31241ad14b2c3
retro <jakedolan443@protonmail.com>
2021-09-27 16:02:55 +0100

    added files

---
+++ download_server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+run_path = os.getcwd()
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    full_path = "{}/static/{}".format(run_path, path)
+    if os.path.isdir(full_path):
+        print(path)
+        files = os.listdir(full_path)
+        data = [path,[]]
+        for f in files:
+            data[1].append({"path":"{}/{}".format(path, f), "end":f, "size":os.path.getsize("{}/{}".format(full_path, f))})
+            if os.path.isdir("{}/{}".format(full_path, f)):
+                data[1][-1]['size'] = "-"
+        return render_template('index.html', data=data)
+    return app.send_static_file(path)
+
+if __name__ == '__main__':
+    app.run()
